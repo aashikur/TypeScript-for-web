54800eb757b2512b947338792154211c07b27239
aashikur <mdaashikur@gmail.com>
2025-11-12 02:25:38 +0600

    function and loop - map

--- FirstTypeScript/src/05_function.ts
+++ FirstTypeScript/src/05_function.ts
@@ -12,3 +12,24 @@ const arrowAdd = ( a : number, b : number) : number => {
 }
 
 console.log(arrowAdd(1, 55))
+
+
+// Object => function => Method
+const user = {
+    name: 'Rifat',
+    baseSalary: 100,
+    getSalary(value : number) {
+        return this.baseSalary + value + value*0.5
+    }
+}
+
+
+let x = user.getSalary(50);
+console.log(x)
+
+
+// map
+
+const arr : number[] = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+const arrSquare = arr.map((item: number) : number => item * item);
+console.log(arrSquare)
