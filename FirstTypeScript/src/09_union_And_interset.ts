f9723549aef1f224945721dddb449c92a15a46e3
aashikur <mdaashikur@gmail.com>
2025-11-17 08:59:32 +0600

    union and interset

--- FirstTypeScript/src/09_union_And_interset.ts
+++ FirstTypeScript/src/09_union_And_interset.ts
@@ -1,3 +1,42 @@
 // Union
 
-type UserRole = 'admin' | 'user' | 'guest'
+type UserRole = 'admin' | 'user' ; // union type
+
+const getDashboard = (role: UserRole) => {
+    if( role == 'admin' ) {
+        console.log('Admin Dashboard')
+    } else if( role == 'user' ) {
+        console.log('User Dashboard')
+    }
+}
+
+getDashboard('admin')
+getDashboard('user')
+// getDashboard('moderator')  // Error will give
+
+
+
+// Intersection
+type Employee = {
+    id: string;
+    name: string;
+    photoNo : string;
+
+}
+
+type Manager = {
+    designation: string;
+    teamSize: number;
+
+}
+
+
+type EmployeeWithManager = Employee & Manager; // manager is also employee 
+
+const MrManager : EmployeeWithManager = {
+    id: '123',
+    name: 'Rifat',
+    photoNo: '123',
+    designation: 'CEO',
+    teamSize: 10
+}
